6b5f60b3f04d7938c9c41c29f5db911d0143124b
Jessica Jimantoro <jessica.jimantoro@dkatalis.com>
2022-01-16 18:23:34 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/video_recorder_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_recorder_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Recording',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Center(),
+    );
+  }
+}

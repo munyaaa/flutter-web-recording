de80c9904df802973c823b459578e0553257ee35
Jessica Jimantoro <jessica.jimantoro@dkatalis.com>
2022-01-16 18:45:56 +0700

    feat: record, stop, display video

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:html' as html;
+import 'dart:js' as js;
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +14,64 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late html.VideoElement _preview;
+  late html.MediaRecorder _recorder;
+  late html.VideoElement _result;
+
+  @override
+  void initState() {
+    super.initState();
+    _preview = html.VideoElement()
+      ..autoplay = true
+      ..muted = true
+      ..width = html.window.innerWidth!
+      ..height = html.window.innerHeight!;
+
+    _result = html.VideoElement()
+      ..autoplay = false
+      ..muted = false
+      ..width = html.window.innerWidth!
+      ..height = html.window.innerHeight!
+      ..controls = true;
+
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory('preview', (int _) => _preview);
+
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory('result', (int _) => _result);
+  }
+
+  Future<html.MediaStream?> _openCamera() async {
+    final html.MediaStream? stream = await html.window.navigator.mediaDevices
+        ?.getUserMedia({'video': true, 'audio': true});
+    _preview.srcObject = stream;
+    return stream;
+  }
+
+  void startRecording(html.MediaStream stream) {
+    _recorder = html.MediaRecorder(stream);
+    _recorder.start();
+
+    html.Blob blob = html.Blob([]);
+
+    _recorder.addEventListener('dataavailable', (event) {
+      blob = js.JsObject.fromBrowserObject(event)['data'];
+    }, true);
+
+    _recorder.addEventListener('stop', (event) {
+      final url = html.Url.createObjectUrl(blob);
+      _result.src = url;
+
+      stream.getTracks().forEach((track) {
+        if (track.readyState == 'live') {
+          track.stop();
+        }
+      });
+    });
+  }
+
+  void stopRecording() => _recorder.stop();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +79,69 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Center(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Web Recording'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Recording Preview',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                width: 300,
+                height: 200,
+                color: Colors.blue,
+                child: HtmlElementView(
+                  key: UniqueKey(),
+                  viewType: 'preview',
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () async {
+                        final html.MediaStream? stream = await _openCamera();
+                        startRecording(stream!);
+                      },
+                      child: Text('Start Recording'),
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => stopRecording(),
+                      child: Text('Stop Recording'),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Recording Result',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                width: 300,
+                height: 200,
+                color: Colors.blue,
+                child: HtmlElementView(
+                  key: UniqueKey(),
+                  viewType: 'result',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
